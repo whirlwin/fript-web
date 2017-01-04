7c94d6e67c74dc7c4791cca6bf03b3ede6ab2dbd
Øyvind Ødegård <oyvind.odegaard@sparebank1.no>
2017-01-04 14:49:48 +0100

    Add migration for gym center preference.

---
+++ db/migrations/20170104134333-create-table-gym-center-preference.js
@@ -0,0 +1,32 @@
+'use strict';
+
+var dbm;
+var type;
+var seed;
+
+/**
+  * We receive the dbmigrate dependency from dbmigrate initially.
+  * This enables us to not have to rely on NODE_PATH.
+  */
+exports.setup = function(options, seedLink) {
+  dbm = options.dbmigrate;
+  type = dbm.dataType;
+  seed = seedLink;
+};
+
+exports.up = function(db, cb) {
+    db.createTable('gym_center_preference', {
+        id: { type: 'int', primaryKey: true, autoIncrement: true },
+        gym_center_id: { type: 'int', notNull: true },
+        created: { type: 'date', notNull: true, defaultValue: 'now()' },
+        updated: { type: 'date', notNull: true, defaultValue: 'now()' }
+    }, cb);
+};
+
+exports.down = function(db, cb) {
+    db.dropTable('gym_center_preference', cb);
+};
+
+exports._meta = {
+  "version": 1
+};
