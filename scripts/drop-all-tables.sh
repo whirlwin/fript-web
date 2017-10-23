cc67c3bf5dbc18134008c4fa44b7bd9b4905e8be
whirlwin <whirlwin90@gmail.com>
2017-10-23 19:46:35 +0200

    Drop all tables for prod with prompt

--- scripts/drop-all-tables.sh
+++ scripts/drop-all-tables.sh
@@ -2,5 +2,18 @@
 
 ENV=$1
 
+if [ ${ENV} == "prod" ]; then
+  echo "Continue? Please type the name of the database"
+  read -p "" CONTINUE
+  if [ "${CONTINUE}" == "fript-production" ]; then
+    DB_NAME="fript-production"
+  else
+    echo "[ABORT]: Not deleting prod database..."
+    exit 0
+  fi
+else
+  DB_NAME="fript-${ENV}"
+fi
+
 cat "./db/st-fixtures/drop-all-tables.sql"\
-    | heroku pg:psql DEV_DB_URL --app fript-staging
+    | heroku pg:psql DEV_DB_URL --app ${DB_NAME}
